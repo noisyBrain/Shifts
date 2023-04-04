b70be5b34cf7d9814f7d3725830c597ca8feb5d3
Tomás <tomasarreguib@gmail.com>
2023-04-04 07:42:26 -0300

    ADD: eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,44 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
   env: {
     node: true,
-    jest: true,
+    es6: true
   },
-  ignorePatterns: ['.eslintrc.js'],
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["@typescript-eslint", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  ignorePatterns: ["*.js", "*.d.ts", "*.json", "schema.prisma", "*.sql", "*.toml", "*.html"],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    "import/prefer-default-export": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": "off",
+    "no-console": ["error", { "allow": ["warn", "error"] }],
+    "no-use-before-define": "off",
+    "no-undef": "off",
+    "no-param-reassign": "off",
+    "no-useless-constructor": "off",
+    "consistent-return": "off",
+    "prettier/prettier": "error",
+    "max-classes-per-file": "off",
+    "class-methods-use-this": "off",
+    "no-underscore-dangle": "off",
+    "no-empty": "off",
+    "no-plusplus": "off",
+    "no-empty-function": "off",
+    "no-unused-vars": "off",
+    "no-shadow": "off",
+    "prefer-destructuring": ["error", { "object": true, "array": false }],
+    "no-restricted-syntax": "off",
+    "no-await-in-loop": "off",
+    "guard-for-in": "off",
+    "no-useless-escape": "off",
+    "default-case": "off",
+    "dot-notation": "off",
+    "@typescript-eslint/no-shadow": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+  }
 };
+
